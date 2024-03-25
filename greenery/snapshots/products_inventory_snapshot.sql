aac156e9434bb2d0742ff131a36770adfa108a91
Josh Paller <josh.paller@vivun.com>
2024-03-24 21:23:38 -0400

    fix snapshot

--- greenery/snapshots/products_inventory_snapshot.sql
+++
@@ -1,9 +0,0 @@
-{{
-  config(
-    target_database = target.database,
-    target_schema = target.schema,
-    strategy='check',
-    unique_key='product_id',
-    check_cols=['inventory'],
-   )
-}}
